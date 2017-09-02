ca6fbf806ce3d587e4dbfb171b198582cc0f5067
Borislav Kavalov <bobby.kavalov@gmail.com>
2017-09-02 23:47:23 +0300

    patternmatcher early

---
+++ patternMatcher.java
@@ -0,0 +1,19 @@
+package homework;
+
+import java.util.Scanner;
+
+public class patternMatcher {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String alo = scan.nextLine();
+		String[] str = alo.split(" ");
+		char[] S = str[0].toCharArray();
+		char[] P = str[1].toCharArray();
+		for(int i = 0; i < S.length; i++){
+			
+		}
+
+	}
+
+}
